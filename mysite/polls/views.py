61719aca4d3b85946e135bc0ecc064175c1610bf
dvstrom <dvstrom@gmail.com>
2017-05-07 20:48:22 +0800

    result 有问题

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from .models import Question, Choice
 from django.template import loader, Template
+from django.views import generic
 
 def index(request):
     # return HttpResponse("Hello, world, you're at the polls index!")
@@ -26,12 +28,42 @@ def detail(request, question_id):
     # except Question.DoesNotExist:
     #     raise Http404("question does not exist!")
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
+    quesiton = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'quesiton': quesiton})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're are voting on question %s" % question_id)
+    # return HttpResponse("You're are voting on question %s" % question_id)
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You don't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_quesiton_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
